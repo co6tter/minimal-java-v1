13f47f24585b8c27622c82600bfd8a5a5df30673
co6tter <co6tter@gmail.com>
2025-05-19 23:41:00 +0900

    chore: add sample

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,5 +11,22 @@ public class Main {
         String firstName = "foo";
         String lastName = "bar";
         System.out.println(String.format("I am %s %s.", firstName, lastName));
+
+        int[] scores = {70, 90, 80, 60};
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
+        for (int score: scores) {
+            if (score >= 80) {
+                break;
+            }
+            System.out.println(score);
+        }
+
+        double money = 500.0;
+        while (money < 1000.0) {
+            money *= 1.1;
+            System.out.println(money);
+        }
     }
 }
